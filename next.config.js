c6547e077251cdaa91ad510f5d01de4c461744d3
Coach Serge N'GUE <sergekoffi@alconsulting-ci.com>
2026-01-02 09:44:31 +0000

    Fix next.config.js & add TypeScript types for Netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // Supprimer experimental.appDir
 };
 
 module.exports = nextConfig;
