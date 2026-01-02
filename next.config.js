c4e2161f44f9e123778682771b9466b5b5759d1c
Coach Serge N'GUE <sergekoffi@alconsulting-ci.com>
2026-01-02 18:30:13 +0000

    Remove experimental appDir from next.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Indispensable pour Netlify avec App Router
-  output: "standalone",
-
-  // Évite certains problèmes de build côté Netlify
-  experimental: {
-    appDir: true,
-  },
-
-  // Images (sécurisé même si tu n’en utilises pas encore)
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
